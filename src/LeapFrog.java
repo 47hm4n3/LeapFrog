0d1614932196f7df1f6439d6c93ca9a7efb4decb
47hm4n3 <pixel@47hm4n3.local>
2018-02-12 15:57:43 +0100

    Now it works - abandonned the idea of exclusive moves (automatically managed by minisat)

--- src/LeapFrog.java
+++ src/LeapFrog.java
@@ -1,37 +1,42 @@
 public class LeapFrog {
 
-	//public static Stone[] stones;
-	public static int num;
+	// public static Stone[] stones;
+	public static int numFrog;
+	public static int numStep;
 	public static int length;
 	public static int empty;
 	public static int step;
-	//public static Stone[] endStones;
+	// public static Stone[] endStones;
 
 	public static void main(String[] args) {
 
-		num = Integer.parseInt(args[0]);
-		length = 2 * num + 1;
+		numFrog = Integer.parseInt(args[0]);
+		numStep = Integer.parseInt(args[1]);
+		numFrog = 10;
+		length = 2 * numFrog + 1;
 		step = 0;
 
 		StringBuilder sb = new StringBuilder("");
 		sb.append(initialize(step));
 		sb.append(" & \n");
-		for (step = 1; step <= 2; step++) {
+		for (step = 1; step <= numStep; step++) {
+			sb.append("\n(\n");
 			sb.append(oneStep(step));
+			sb.append("\n)\n");
 			sb.append(" & \n");
 			sb.append(oneStoneTwoFrogs(step));
 			sb.append(" & ");
 		}
-		sb.append(end(step-1));
+		sb.append(end(step - 1));
 		System.out.println(sb.toString());
 	}
 
 	public static String oneStep(int s) {
 		StringBuilder sb = new StringBuilder("");
 		for (int i = 0; i < length; i++) {
-			sb.append("(");
+			sb.append("\n(\n");
 			sb.append(moveOneBox(s, i));
-			sb.append(")");
+			sb.append("\n)\n");
 			sb.append(" | \n");
 		}
 		sb.setLength(sb.length() - 4);
@@ -43,91 +48,22 @@ public class LeapFrog {
 		String str1 = jumpToRight(s, i);
 		sb.append(str1);
 		if (!str1.isEmpty()) {
-			sb.append(" & ");
-			sb.append("\n!(");
-			String str = jumpToLeft(s, i);
-			sb.append(str);
-			if (!str.isEmpty()) 
-				sb.append(" | \n");
-			str = jumpOverRight(s, i);
-			sb.append(str);
-			if (!str.isEmpty()) 
-				sb.append(" | \n");
-			str = jumpOverLeft(s, i);
-			if (str.isEmpty()) 
-				sb.setLength(sb.length() - 4);
-			else 
-				sb.append(str);
-			sb.append(")");
 			sb.append(" | \n");
 		}
-		//sb.setLength(sb.length() - 4);
 		str1 = jumpToLeft(s, i);
 		sb.append(str1);
 		if (!str1.isEmpty()) {
-			sb.append(" & ");
-			sb.append("\n!(");
-			String str = jumpToRight(s, i);
-			sb.append(str);
-			if (!str.isEmpty()) 
-				sb.append(" | \n");
-			str = jumpOverRight(s, i);
-			sb.append(str);
-			if (!str.isEmpty()) 
-				sb.append(" | \n");
-			str = jumpOverLeft(s, i);
-			if (str.isEmpty()) 
-				sb.setLength(sb.length() - 4);
-			else 
-				sb.append(str);
-			sb.append(")");
 			sb.append(" | \n");
 		}
-		//sb.setLength(sb.length() - 4);
 		str1 = jumpOverRight(s, i);
 		sb.append(str1);
 		if (!str1.isEmpty()) {
-			sb.append(" & ");
-			sb.append("\n!(");
-			String str = jumpToRight(s, i);
-			sb.append(str);
-			if (!str.isEmpty()) 
-				sb.append(" | \n");
-			str = jumpToLeft(s, i);
-			sb.append(str);
-			if (!str.isEmpty()) 
-				sb.append(" | \n");
-			str = jumpOverLeft(s, i);
-			if (str.isEmpty()) 
-				sb.setLength(sb.length() - 4);
-			else 
-				sb.append(str);
-			sb.append(")");
 			sb.append(" | \n");
 		}
-		sb.setLength(sb.length() - 4);
 		str1 = jumpOverLeft(s, i);
 		if (str1.isEmpty()) {
 			sb.setLength(sb.length() - 4);
 		} else {
-			sb.append(" & ");
-			sb.append("\n!(");
-			String str = jumpToLeft(s, i);
-			sb.append(str);
-			if (!str.isEmpty()) 
-				sb.append(" | \n");
-			str = jumpOverRight(s, i);
-			sb.append(str);
-			if (!str.isEmpty()) 
-				sb.append(" | \n");
-			str = jumpOverLeft(s, i);
-			if (str.isEmpty()) 
-				sb.setLength(sb.length() - 4);
-			else 
-				sb.append(str);
-			sb.append(")");
-			sb.append(" & ");
-
 			sb.append(str1);
 		}
 		return sb.toString();
@@ -136,11 +72,13 @@ public class LeapFrog {
 	public static String jumpToRight(int s, int i) {
 		StringBuilder sb = new StringBuilder("");
 		if ((i + 1) < length) {
+			sb.append(" ( ");
 			sb.append("(x" + i + "_" + (s - 1) + " & " + "!y" + i + "_" + (s - 1) + ")");
 			sb.append(" & ");
 			sb.append("(!x" + (i + 1) + "_" + (s - 1) + " & " + "!y" + (i + 1) + "_" + (s - 1) + ")");
 			sb.append(" & ");
 			sb.append(toBool(s, i, i + 1));
+			sb.append(" ) ");
 		} else {
 			sb.append("");
 		}
@@ -150,11 +88,13 @@ public class LeapFrog {
 	public static String jumpToLeft(int s, int i) {
 		StringBuilder sb = new StringBuilder("");
 		if ((i - 1) >= 0) {
+			sb.append(" ( ");
 			sb.append("(!x" + i + "_" + (s - 1) + " & " + "y" + i + "_" + (s - 1) + ")");
 			sb.append(" & ");
 			sb.append("(!x" + (i - 1) + "_" + (s - 1) + " & " + "!y" + (i - 1) + "_" + (s - 1) + ")");
 			sb.append(" & ");
 			sb.append(toBool(s, i - 1, i));
+			sb.append(" ) ");
 		} else {
 			sb.append("");
 		}
@@ -164,6 +104,7 @@ public class LeapFrog {
 	public static String jumpOverRight(int s, int i) {
 		StringBuilder sb = new StringBuilder("");
 		if ((i + 2) < length) {
+			sb.append(" ( ");
 			sb.append("(x" + i + "_" + (s - 1) + " & " + "!y" + i + "_" + (s - 1) + ")");
 			sb.append(" & ");
 			sb.append("(!x" + (i + 1) + "_" + (s - 1) + " & " + "y" + (i + 1) + "_" + (s - 1) + ")");
@@ -171,6 +112,7 @@ public class LeapFrog {
 			sb.append("(!x" + (i + 2) + "_" + (s - 1) + " & " + "!y" + (i + 2) + "_" + (s - 1) + ")");
 			sb.append(" & ");
 			sb.append(toBool(s, i, i + 2));
+			sb.append(" ) ");
 		} else {
 			sb.append("");
 		}
@@ -180,6 +122,7 @@ public class LeapFrog {
 	public static String jumpOverLeft(int s, int i) {
 		StringBuilder sb = new StringBuilder("");
 		if ((i - 2) >= 0) {
+			sb.append(" ( ");
 			sb.append("(!x" + i + "_" + (s - 1) + " & " + "y" + i + "_" + (s - 1) + ")");
 			sb.append(" & ");
 			sb.append("(x" + (i - 1) + "_" + (s - 1) + " & " + "!y" + (i - 1) + "_" + (s - 1) + ")");
@@ -187,6 +130,7 @@ public class LeapFrog {
 			sb.append("(!x" + (i - 2) + "_" + (s - 1) + " & " + "!y" + (i - 2) + "_" + (s - 1) + ")");
 			sb.append(" & ");
 			sb.append(toBool(s, i - 2, i));
+			sb.append(" ) ");
 		} else {
 			sb.append("");
 		}
@@ -195,7 +139,7 @@ public class LeapFrog {
 
 	public static String initialize(int s) {
 		StringBuilder sb = new StringBuilder("");
-		sb.append("(");
+		sb.append("\n(\n");
 		for (int j = 0; j < length / 2; j++) {
 			sb.append("(x" + j + "_" + s + " & " + "!y" + j + "_" + s + ")");
 			sb.append(" & ");
@@ -207,13 +151,13 @@ public class LeapFrog {
 			sb.append(" & ");
 		}
 		sb.setLength(sb.length() - 3);
-		sb.append(")");
+		sb.append("\n)\n");
 		return sb.toString();
 	}
 
 	public static String end(int s) {
 		StringBuilder sb = new StringBuilder("");
-		sb.append("(");
+		sb.append("\n(\n");
 		for (int j = 0; j < length / 2; j++) {
 			sb.append("(!x" + j + "_" + s + " & " + "y" + j + "_" + s + ")");
 			sb.append(" & ");
@@ -225,32 +169,30 @@ public class LeapFrog {
 			sb.append(" & ");
 		}
 		sb.setLength(sb.length() - 3);
-		sb.append(")");
+		sb.append("\n)\n");
 		return sb.toString();
 	}
 
 	public static String oneStoneTwoFrogs(int s) {
 		StringBuilder sb = new StringBuilder("");
-		sb.append("(");
+		sb.append("\n(\n");
 		for (int j = 0; j < length; j++) {
 			sb.append("!(x" + j + "_" + s + " & " + "y" + j + "_" + s + ")");
 			sb.append(" & ");
 		}
 		sb.setLength(sb.length() - 3);
-		sb.append(")");
+		sb.append("\n)\n");
 		return sb.toString();
 	}
 
-	/*public static void change(Stone[] tab, int i, int j) {
-		Stone tmp = new Stone();
-		tmp = tab[i];
-		tab[i] = tab[j];
-		tab[j] = tmp;
-	}*/
+	/*
+	 * public static void change(Stone[] tab, int i, int j) { Stone tmp = new
+	 * Stone(); tmp = tab[i]; tab[i] = tab[j]; tab[j] = tmp; }
+	 */
 
 	public static String toBool(int s, int min, int max) {
 		StringBuilder sb = new StringBuilder("");
-		sb.append("(");
+		// sb.append("(");
 		switch (max - min) {
 		case 1:
 			for (int j = 0; j < min; j++) {
@@ -295,33 +237,21 @@ public class LeapFrog {
 			break;
 		}
 		sb.setLength(sb.length() - 3);
-		sb.append(")");
+		// sb.append(")");
 		return sb.toString();
 	}
 
-	/*public static String arrayToString(Stone[] tab) {
-		StringBuilder sb1 = new StringBuilder("");
-		StringBuilder sb2 = new StringBuilder("");
-
-		sb1.append("| ");
-		sb2.append("| ");
-
-		for (int i = 0; i < tab.length; i++) {
-			if (tab[i].x)
-				sb1.append(tab[i].x + " ");
-			else
-				sb1.append(tab[i].x);
-			sb1.append(" | ");
-			if (tab[i].y)
-				sb2.append(tab[i].y + " ");
-			else
-				sb2.append(tab[i].y);
-			sb2.append(" | ");
-		}
-		sb1.append("\n");
-		sb1.append(sb2.toString());
-		sb1.append("\n");
-		return sb1.toString();
-	}*/
+	/*
+	 * public static String arrayToString(Stone[] tab) { StringBuilder sb1 = new
+	 * StringBuilder(""); StringBuilder sb2 = new StringBuilder("");
+	 * 
+	 * sb1.append("| "); sb2.append("| ");
+	 * 
+	 * for (int i = 0; i < tab.length; i++) { if (tab[i].x) sb1.append(tab[i].x
+	 * + " "); else sb1.append(tab[i].x); sb1.append(" | "); if (tab[i].y)
+	 * sb2.append(tab[i].y + " "); else sb2.append(tab[i].y); sb2.append(" | ");
+	 * } sb1.append("\n"); sb1.append(sb2.toString()); sb1.append("\n"); return
+	 * sb1.toString(); }
+	 */
 
 }
